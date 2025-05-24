646d2e47d4f4adcb5d53d7764c320ae2a93f32f3
VitorioBearari <vitorio.bearari@sptech.school>
2025-05-24 08:54:37 -0300

    Versao 2 do quiz inserindo direto no banco

--- Site/API/src/database/script-fossilfacil.sql
+++ Site/API/src/database/script-fossilfacil.sql
@@ -1,33 +1,5 @@
 CREATE DATABASE IF NOT EXISTS FossilFacil;
-/*
-USE FossilFacil;
 
-CREATE TABLE usuario(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100),
-    email VARCHAR(200),
-    senha VARCHAR(100)
-);
-
-CREATE TABLE resultadoQuiz (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fkUsuario INT,
-    acertos INT,
-    data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (fkUsuario) REFERENCES usuario(id)
-);
-
-CREATE TABLE respostasUsuario (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fkResultadoQuiz INT,
-    perguntaIndex INT,
-    respostaDada VARCHAR(100),
-    isCorreta BOOLEAN,
-    FOREIGN KEY (fkResultadoQuiz) REFERENCES ResultadoQuiz(id)
-);
-
-SELECT * FROM resultadoQuiz;
-*/
 USE FossilFacil ;
 
 CREATE TABLE IF NOT EXISTS FossilFacil.usuario (
@@ -57,6 +29,6 @@ CREATE TABLE IF NOT EXISTS FossilFacil.respostasUsuario (
   FOREIGN KEY (fkResultadoQuiz) REFERENCES FossilFacil.resultadoQuiz (id)
 );	
 
-SELECT * FROM resultadoQuiz;
-SELECT * FROM usuario;
+SELECT * FROM FossilFacil.usuario;
+SELECT * FROM FossilFacil.resultadoQuiz;
 SELECT * FROM FossilFacil.respostasUsuario;
--- Site/API/src/models/quizModel.js
+++ Site/API/src/models/quizModel.js
@@ -9,27 +9,21 @@ function salvarResultadoCompleto(idUsuario, acertos, respostas) {
     console.log("Inserindo resultado no banco...");
 
     // Primeiro salva a tentativa na Resultado
-    return database.executar(inserirResultadoQuery)
-        .then(resultado => {
-            // Depois de inserir, precisamos saber o ID que foi gerado
-            const resultadoIdQuery = `SELECT LAST_INSERT_ID() as id;`;
-            return database.executar(resultadoIdQuery);
-        })
-        .then(resultadoConsulta => {
-            const idResultadoQuiz = resultadoConsulta[0].id;
+   return database.executar(inserirResultadoQuery)
+    .then(resultado => {
+        const idResultadoQuiz = resultado.insertId;
 
-            // Agora montamos os comandos para salvar as 10 respostas
-            const respostasInserts = respostas.map(resp => {
-                return `
-                    INSERT INTO respostasUsuario (id, perguntaIndex, respostaDada, isCorreta)
-                    VALUES (${idResultadoQuiz}, ${resp.perguntaIndex}, '${resp.respostaDada}', ${resp.isCorreta});
-                `;
-            });
-
-            // Executa todos os inserts (um para cada resposta)
-            const promessas = respostasInserts.map(instrucao => database.executar(instrucao));
-            return Promise.all(promessas);
+        const respostasInserts = respostas.map(resp => {
+            return `
+                INSERT INTO respostasUsuario (fkResultadoQuiz, perguntaIndex, respostaDada, isCorreta)
+                VALUES (${idResultadoQuiz}, ${resp.perguntaIndex}, '${resp.respostaDada}', ${resp.isCorreta});
+            `;
         });
+
+        const promessas = respostasInserts.map(instrucao => database.executar(instrucao));
+        return Promise.all(promessas);
+    });
+ 
 }
 
 module.exports = {
